05b99ce06ccb4858b786ba3e671c14d5ecb79081
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-08 09:59:15 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import styles from "./styles.css";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
