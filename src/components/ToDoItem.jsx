5fe867634457ca5309790c3c62e8aff5727508fc
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-08 10:29:09 +0530

    ntng

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,9 +1,19 @@
-import react from "react";
+import React, { useState } from "react";
 
 function ToDoItem(props) {
+  const [isDone, setIsDone] = useState(false);
+
+  function handleClick() {
+    setIsDone((preValue) => {
+      return !preValue;
+    });
+  }
+
   return (
-    <div>
-      <li>{props.text}</li>
+    <div onClick={handleClick}>
+      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+        {props.text}
+      </li>
     </div>
   );
 }
